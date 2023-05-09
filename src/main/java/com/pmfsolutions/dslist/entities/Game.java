29d7847e2d4a6ea9e774ce3ba0a73fd9c7e3147e
Pedro Sobral <pedrosobral2009@gmail.com>
2023-05-09 09:58:15 -0300

    Game seeds

--- src/main/java/com/pmfsolutions/dslist/entities/Game.java
+++ src/main/java/com/pmfsolutions/dslist/entities/Game.java
@@ -7,7 +7,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Table(name="tb_game")
 @Entity
 public class Game {
     
@@ -18,7 +20,7 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String plataforms;
+    private String platforms;
     private Double score;
     private String imgUrl;
     @Column(columnDefinition = "TEXT")
@@ -29,13 +31,13 @@ public class Game {
     public Game() {
     }
     
-    public Game(Long id, String title, Integer year, String genre, String plataforms, Double score, String imgUrl,
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
             String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.plataforms = plataforms;
+        this.platforms = platforms;
         this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
@@ -68,10 +70,10 @@ public class Game {
         this.genre = genre;
     }
     public String getPlataforms() {
-        return plataforms;
+        return platforms;
     }
     public void setPlataforms(String plataforms) {
-        this.plataforms = plataforms;
+        this.platforms = plataforms;
     }
     public Double getScore() {
         return score;
