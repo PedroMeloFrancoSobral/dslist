fb6fd460af65b526143022dd7cdb648a3bac43fa
Pedro Sobral <pedrosobral2009@gmail.com>
2023-05-10 14:37:13 -0300

    GET game by id

--- src/main/java/com/pmfsolutions/dslist/services/GameService.java
+++ src/main/java/com/pmfsolutions/dslist/services/GameService.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.pmfsolutions.dslist.repositories.GameRepository;
+import com.pmfsolutions.dslist.dto.GameDTO;
 import com.pmfsolutions.dslist.dto.GameMinDTO;
 import com.pmfsolutions.dslist.entities.Game;
 
@@ -15,8 +17,15 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly=true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
         return result.stream().map(x-> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly=true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
     }
 }
